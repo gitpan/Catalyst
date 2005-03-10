17f1948ea94dc04f33eeba50c18ea633b6158938
Sebastian Riedel <kraihx@googlemail.com>
2005-03-10 21:43:39 +0000

    Import of SRI/Catalyst-4.23 from CPAN.
    gitpan-cpan-distribution: Catalyst
    gitpan-cpan-version:      4.23
    gitpan-cpan-path:         SRI/Catalyst-4.23.tar.gz
    gitpan-cpan-author:       SRI
    gitpan-cpan-maturity:     released

---
+++ script/catalyst.pl
@@ -0,0 +1,62 @@
+#!/usr/bin/perl -w
+
+use strict;
+use Getopt::Long;
+use Pod::Usage;
+use Catalyst::Helper;
+
+my $help = 0;
+
+GetOptions( 'help|?' => \$help );
+
+pod2usage(1) if ( $help || !$ARGV[0] );
+
+my $helper = Catalyst::Helper->new;
+pod2usage(1) unless $helper->mk_app( $ARGV[0] );
+
+1;
+__END__
+
+=head1 NAME
+
+catalyst - Bootstrap a Catalyst application
+
+=head1 SYNOPSIS
+
+catalyst.pl [options] application-name
+
+ Options:
+   -help        display this help and exits
+
+ application-name has to be a valid Perl module name and can include ::
+
+ Examples:
+    catalyst.pl My::App
+    catalyst.pl MyApp
+
+ See also:
+    perldoc Catalyst::Manual
+    perldoc Catalyst::Manual::Intro
+
+=head1 DESCRIPTION
+
+Bootstrap a Catalyst application.
+
+=head1 SEE ALSO
+
+L<Catalyst>, L<Catalyst::Manual>, L<Catalyst::Manual::Intro>,
+L<Catalyst::Test>, L<Catalyst::Request>, L<Catalyst::Response>,
+L<Catalyst::Engine>
+
+=head1 AUTHOR
+
+Sebastian Riedel Sebastian Riedel, C<sri@oook.de>
+
+=head1 COPYRIGHT
+
+Copyright 2004 Sebastian Riedel. All rights reserved.
+
+This library is free software. You can redistribute it and/or modify it under
+the same terms as perl itself.
+
+=cut
