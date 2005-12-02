8f85cdde3e4b52e552e0343e045cb693caa68805
Sebastian Riedel <kraihx@googlemail.com>
2005-12-02 05:18:26 +0000

    Import of SRI/Catalyst-5.61 from CPAN.
    gitpan-cpan-distribution: Catalyst
    gitpan-cpan-version:      5.61
    gitpan-cpan-path:         SRI/Catalyst-5.61.tar.gz
    gitpan-cpan-author:       SRI
    gitpan-cpan-maturity:     released

--- inc/ExtUtils/AutoInstall.pm
+++ inc/ExtUtils/AutoInstall.pm
@@ -1,4 +1,4 @@
-#line 1 "inc/ExtUtils/AutoInstall.pm - /usr/lib/perl5/site_perl/5.8.6/ExtUtils/AutoInstall.pm"
+#line 1 "inc/ExtUtils/AutoInstall.pm - /Library/Perl/5.8.6/ExtUtils/AutoInstall.pm"
 package ExtUtils::AutoInstall;
 $ExtUtils::AutoInstall::VERSION = '0.63';
 
